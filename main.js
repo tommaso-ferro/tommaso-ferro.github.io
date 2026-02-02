a1979fcac251f846259fcf425dbfa7bc6c9a08b2
Ferro Tommaso <tommaso.ferro@cz.lan>
2026-02-02 09:45:13 +0100

    updated website

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
+const button = document.querySelector('#sendButton');
+const name = document.querySelector('#name');
+const email = document.querySelector('#email');
+const message = document.querySelector('#message');
+const next = document.querySelector('#next');
+const prev = document.querySelector('#prev');
+
 function main() {
     console.log("It Works!");
 }
@@ -12,3 +19,9 @@ if ('scrollRestoration' in history) {
 function initScroll() {
     window.scrollTo(0, 0);
 }
+
+button.addEventListener('click', () => {
+    console.log("Name: ", name.value);
+    console.log("Email: ", email.value);
+    console.log("Message: ", message.value);
+});
